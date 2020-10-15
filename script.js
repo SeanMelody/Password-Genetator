1ebeccec73c93b8f27c8085b88a7e6657b1b6213
Sean Melody <seandmelody@gmail.com>
2020-10-15 14:04:36 -0700

    Added Lowercase letter option

--- script.js
+++ script.js
@@ -41,6 +41,26 @@ function promptBtn() {
   console.log(upperPassword);
 
 
+  // LowerCase Letters
+
+  // Create and Empty String to have Lowercase Password Options
+  var lowerPassword = ""
+  // Confirm for an alert asking if you want Lowercase Letters
+  let passLower = confirm("Would you like your password to contain lowercase letters?");
+  // If Statement for if they Want Lowercase Letters, and then Randomize Those Letters
+  if (passLower === true) {
+    var lowerCharOptions = "abcdefghijklmnopqrstuvwxyz";
+    // Randomize the Order of the Lowercase Alphabet
+    for (let i = 0; i <= 26; i++) {
+      let lowerChar = lowerCharOptions.charAt(Math.floor(Math.random() * 26));
+      lowerPassword = (lowerChar + lowerPassword);
+    }
+    // Else Statement for if NO Lowercase letter is Wanted
+  } else {
+    alert("Your password will not have any lower case letters");
+  }
+
+  console.log(lowerPassword);
 
 
   return (buttonPress)
